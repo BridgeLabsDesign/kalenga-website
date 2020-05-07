4f289be9fb30e2515b7605e84e04da51a52de815
Tumelo Makgaka <mailing.tumelo@gmail.com>
2020-05-08 01:38:40 +0200

    changed home tiles

--- js/grayscale.js
+++ js/grayscale.js
@@ -45,26 +45,32 @@
 
 
   var Shuffle = window.Shuffle;
-var element = document.querySelector('.my-shuffle-container');
-var sizer = element.querySelector('.my-sizer-element');
+  var element = document.querySelector('.my-shuffle-container');
+  var sizer = element.querySelector('.my-sizer-element');
 
-var shuffleInstance = new Shuffle(element, {
-  itemSelector: '.picture-item',
-  sizer: sizer // could also be a selector: '.my-sizer-element'
-});
-// shuffleInstance.filter('animal');
-$("#all").on("click", function(){
-   shuffleInstance.filter();
-});
-$("#btn-linkedin").on("click", function(){
-   shuffleInstance.filter('linkedin');
-});
-$("#btn-youtube").on("click", function(){
-   shuffleInstance.filter('youtube');
-});
-$("#btn-twitter").on("click", function(){
-   shuffleInstance.filter('twitter');
-});
+  var shuffleInstance = new Shuffle(element, {
+    itemSelector: '.picture-item',
+    sizer: sizer // could also be a selector: '.my-sizer-element'
+  });
+  // shuffleInstance.filter('animal');
+  $("#all").on("click", function () {
+    shuffleInstance.filter();
+  });
+  $("#btn-linkedin").on("click", function () {
+    shuffleInstance.filter('linkedin');
+  });
+  $("#btn-youtube").on("click", function () {
+    shuffleInstance.filter('youtube');
+  });
+  $("#btn-twitter").on("click", function () {
+    shuffleInstance.filter('twitter');
+  });
+  $("#btn-soundcloud").on("click", function () {
+    shuffleInstance.filter('soundcloud');
+  });
+  $("#btn-medium").on("click", function () {
+    shuffleInstance.filter('medium');
+  });
 
 
 
